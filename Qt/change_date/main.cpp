f78c7c2b81ac9ae928322d4cbd935d29feafe715
cherrydan <cherrydan.1978@gmail.com>
2018-09-13 14:26:11 +0300

    New pointer example

---
+++ Qt/change_date/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+void change_date(int *d, int *m, int *y) {
+    *d = *d;
+    *m = *m;
+    *y = *y;
+}
+
+int main()
+{
+    int d, m, y;
+
+    d = 25;
+    m = 10;
+    y = 2017;
+
+    //Передаём адреса трех int-ов и получаем правильную дату
+    change_date(&d, &m, &y);
+
+    cout << "d = " << d << " m = " << m << " y = " << y;
+
+
+    return 0;
+}
