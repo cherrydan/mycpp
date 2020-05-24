bc8c5f99d16c1a97058d79b584bfce8575800641
Danil <cherrydan.1978@gmail.com>
2020-05-24 16:02:53 +0300

    Продолжаем урок2 по Си

--- LessonsC/prog1/main.c
+++ LessonsC/prog1/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("48398");
+    int t;
+    t = 15;
+    t = (t - 2) * t + 5;
+    float v = 2.54;
+    printf("t = %d decimal, t = %x hex\n", t, t);
+    printf("%.2f\n", v);
+    v = t;
+    printf("%g", v);
+
 }
