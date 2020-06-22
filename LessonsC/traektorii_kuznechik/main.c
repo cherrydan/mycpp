a26f22414cc5c2d38a87e222d7f17720779790f5
Danil <cherrydan.1978@gmail.com>
2020-06-22 12:07:27 +0300

    Траектории Кузнечика (Тимофей Хирьянов)

--- LessonsC/traektorii_kuznechik/main.c
+++ LessonsC/traektorii_kuznechik/main.c
@@ -3,9 +3,22 @@
  Траектории кузнечика
  (Тимофей Хирьянов
 */
+int number_of_trajectories(int n)
+{
+    int K[n+1];
+    K[0] = 0;
+    K[1] = 1;
+    for(int i = 2; i <= n; i++)
+        K[i] = K[i-1] + K[i - 2];
+    return K[n];
+}
+
+
 
 int main()
 {
-
+    int finish;
+    scanf("%d", &finish);
+    printf("Кузнечик имеет %d траекторий из 1 в %d\n", number_of_trajectories(finish), finish);
     return 0;
 }
